57a3d6e707ebdc6c0f788f1dd1399aa3b8d41539
arthurshafikov <arthur.shafikov27@gmail.com>
2022-04-27 05:37:24 +0400

    add Game model

---
+++ internal/core/game.go
@@ -0,0 +1,12 @@
+package core
+
+type Game struct {
+	Deck         Cards                 `bson:"deck"`
+	DealerHand   PlayerHand            `bson:"dealer_hand"`
+	PlayersHands map[string]PlayerHand `bson:"players_hand"`
+}
+
+type PlayerHand struct {
+	Cards Cards `bson:"cards"`
+	Stop  bool  `bson:"stop"`
+}
