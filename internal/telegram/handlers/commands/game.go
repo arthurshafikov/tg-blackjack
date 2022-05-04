8593dc67983cd53a34969f2811d38f265652dc83
arthurshafikov <arthur.shafikov27@gmail.com>
2022-05-04 08:21:25 +0400

    add hint when game is finished

--- internal/telegram/handlers/commands/game.go
+++ internal/telegram/handlers/commands/game.go
@@ -67,6 +67,8 @@ func (c *CommandHandler) finishGameIfNeeded(message *tgbotapi.Message) error {
 			msgText += fmt.Sprintf("\n@%s - %s", c.escapeUnderscoreUsername(username), resultText)
 		}
 
+		msgText += fmt.Sprintf("\n\n Type /%s to start a new game", core.NewGame)
+
 		return c.sendMessage(tgbotapi.NewMessage(message.Chat.ID, msgText))
 	}
 
