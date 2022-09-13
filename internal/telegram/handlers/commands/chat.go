e5473b0b91f807f86b2cd7cc4c596eb338b067a8
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-13 07:37:15 +0400

    move HandleStart to chat.go commandHandler

--- internal/telegram/handlers/commands/chat.go
+++ internal/telegram/handlers/commands/chat.go
@@ -1,10 +1,26 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/arthurshafikov/tg-blackjack/internal/core"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
+func (c *CommandHandler) HandleStart(message *tgbotapi.Message) error {
+	if err := c.services.Chats.RegisterChat(c.ctx, message.Chat.ID); err != nil {
+		if errors.Is(err, core.ErrChatRegistered) {
+			return fmt.Errorf(c.messages.ChatAlreadyRegistered)
+		}
+
+		return err
+	}
+
+	msg := c.helper.NewMessage(message.Chat.ID, c.messages.ChatCreatedSuccessfully)
+
+	return c.helper.SendMessage(msg)
+}
 
 func (c *CommandHandler) HandleStats(message *tgbotapi.Message) error {
 	stats, err := c.services.Statistics.GetStatistics(c.ctx, message.Chat.ID)
--- internal/telegram/handlers/commands/command_handler.go
+++ internal/telegram/handlers/commands/command_handler.go
@@ -2,15 +2,11 @@ package commands
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"strings"
 
 	"github.com/arthurshafikov/tg-blackjack/internal/config"
-	"github.com/arthurshafikov/tg-blackjack/internal/core"
 	"github.com/arthurshafikov/tg-blackjack/internal/services"
 	"github.com/arthurshafikov/tg-blackjack/internal/telegram/handlers"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 type CommandHandler struct {
@@ -30,20 +26,6 @@ func NewCommandHandler(handlerParams handlers.HandlerParams) *CommandHandler {
 
 		helper: handlerParams.Helper,
 	}
-}
-
-func (c *CommandHandler) HandleStart(message *tgbotapi.Message) error {
-	if err := c.services.Chats.RegisterChat(c.ctx, message.Chat.ID); err != nil {
-		if errors.Is(err, core.ErrChatRegistered) {
-			return fmt.Errorf(c.messages.ChatAlreadyRegistered)
-		}
-
-		return err
-	}
-
-	msg := c.helper.NewMessage(message.Chat.ID, c.messages.ChatCreatedSuccessfully)
-
-	return c.helper.SendMessage(msg)
 }
 
 func (c *CommandHandler) escapeUnderscoreUsername(username string) string {
