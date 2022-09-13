b72b7f14445709d9773f0542107f9e87c9736f1e
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-13 08:05:40 +0400

    add commandHandler chat unit tests

---
+++ internal/telegram/handlers/commands/chat_test.go
@@ -0,0 +1,107 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/arthurshafikov/tg-blackjack/internal/core"
+	"github.com/arthurshafikov/tg-blackjack/internal/telegram/handlers"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	chatID      int64 = 28
+	receivedMsg       = &tgbotapi.Message{
+		Text: "Some message",
+		Chat: &tgbotapi.Chat{
+			ID: chatID,
+		},
+	}
+	stats = core.UsersStatistics{
+		"someUser1": 22,
+		"someUser2": -4,
+	}
+)
+
+func TestHandleStart(t *testing.T) {
+	handlerParams, mockBag := handlers.GetHandlerParamsWithMocks(t)
+	c := NewCommandHandler(handlerParams)
+	responseConfig := tgbotapi.NewMessage(chatID, c.messages.ChatCreatedSuccessfully)
+	gomock.InOrder(
+		mockBag.Chats.EXPECT().RegisterChat(c.ctx, chatID).Return(nil),
+		mockBag.Helper.EXPECT().NewMessage(chatID, c.messages.ChatCreatedSuccessfully).Return(responseConfig),
+		mockBag.Helper.EXPECT().SendMessage(responseConfig).Return(nil),
+	)
+
+	err := c.HandleStart(receivedMsg)
+	require.NoError(t, err)
+}
+
+func TestHandleStartChatRegistered(t *testing.T) {
+	handlerParams, mockBag := handlers.GetHandlerParamsWithMocks(t)
+	c := NewCommandHandler(handlerParams)
+	gomock.InOrder(
+		mockBag.Chats.EXPECT().RegisterChat(c.ctx, chatID).Return(core.ErrChatRegistered),
+	)
+
+	err := c.HandleStart(receivedMsg)
+	require.ErrorContains(t, err, c.messages.ChatAlreadyRegistered)
+}
+
+func TestHandleStartSendMessageReturnsError(t *testing.T) {
+	handlerParams, mockBag := handlers.GetHandlerParamsWithMocks(t)
+	c := NewCommandHandler(handlerParams)
+	responseConfig := tgbotapi.NewMessage(chatID, c.messages.ChatCreatedSuccessfully)
+	gomock.InOrder(
+		mockBag.Chats.EXPECT().RegisterChat(c.ctx, chatID).Return(nil),
+		mockBag.Helper.EXPECT().NewMessage(chatID, c.messages.ChatCreatedSuccessfully).Return(responseConfig),
+		mockBag.Helper.EXPECT().SendMessage(responseConfig).Return(core.ErrServerError),
+		mockBag.Logger.EXPECT().Error(gomock.Any()),
+	)
+
+	err := c.HandleStart(receivedMsg)
+	require.ErrorIs(t, err, core.ErrServerError)
+}
+
+func TestHandleStats(t *testing.T) {
+	handlerParams, mockBag := handlers.GetHandlerParamsWithMocks(t)
+	c := NewCommandHandler(handlerParams)
+	responseConfig := tgbotapi.NewMessage(chatID, c.messages.ChatCreatedSuccessfully)
+	gomock.InOrder(
+		mockBag.Statistics.EXPECT().GetStatistics(c.ctx, chatID).Return(stats, nil),
+		mockBag.Helper.EXPECT().NewMessage(chatID, "TopPlayers\n\n*1.* @someUser1 — 22 points\n*2.* @someUser2 — -4 points\n").
+			Return(responseConfig),
+		mockBag.Helper.EXPECT().SendMessage(responseConfig).Return(nil),
+	)
+
+	err := c.HandleStats(receivedMsg)
+	require.NoError(t, err)
+}
+
+func TestHandleStatsGetStatisticsReturnsError(t *testing.T) {
+	handlerParams, mockBag := handlers.GetHandlerParamsWithMocks(t)
+	c := NewCommandHandler(handlerParams)
+	gomock.InOrder(
+		mockBag.Statistics.EXPECT().GetStatistics(c.ctx, chatID).Return(nil, core.ErrServerError),
+	)
+
+	err := c.HandleStats(receivedMsg)
+	require.ErrorIs(t, err, core.ErrServerError)
+}
+
+func TestHandleStatsSendMessageReturnsError(t *testing.T) {
+	handlerParams, mockBag := handlers.GetHandlerParamsWithMocks(t)
+	c := NewCommandHandler(handlerParams)
+	responseConfig := tgbotapi.NewMessage(chatID, c.messages.ChatCreatedSuccessfully)
+	gomock.InOrder(
+		mockBag.Statistics.EXPECT().GetStatistics(c.ctx, chatID).Return(stats, nil),
+		mockBag.Helper.EXPECT().NewMessage(chatID, "TopPlayers\n\n*1.* @someUser1 — 22 points\n*2.* @someUser2 — -4 points\n").
+			Return(responseConfig),
+		mockBag.Helper.EXPECT().SendMessage(responseConfig).Return(core.ErrServerError),
+		mockBag.Logger.EXPECT().Error(gomock.Any()),
+	)
+
+	err := c.HandleStats(receivedMsg)
+	require.ErrorIs(t, err, core.ErrServerError)
+}
---
+++ internal/telegram/handlers/tests.go
@@ -0,0 +1,82 @@
+package handlers
+
+import (
+	"context"
+	"testing"
+
+	"github.com/arthurshafikov/tg-blackjack/internal/config"
+	"github.com/arthurshafikov/tg-blackjack/internal/services"
+	mock_services "github.com/arthurshafikov/tg-blackjack/internal/services/mocks"
+	mock_handlers "github.com/arthurshafikov/tg-blackjack/internal/telegram/handlers/mocks"
+	"github.com/golang/mock/gomock"
+)
+
+var messages = config.Messages{
+	ChatAlreadyRegistered:   "ChatAlreadyRegistered",
+	ChatNotExists:           "ChatNotExists",
+	ChatCreatedSuccessfully: "ChatCreatedSuccessfully",
+	ChatHasActiveGame:       "ChatHasActiveGame",
+	ChatHasNoActiveGame:     "ChatHasNoActiveGame",
+	Blackjack:               "Blackjack",
+	GameOver:                "GameOver",
+	Win:                     "Win",
+	Lose:                    "Lose",
+	Push:                    "Push",
+	BlackjackResult:         "BlackjackResult",
+	PlayerCantDraw:          "PlayerCantDraw",
+	PlayerHand:              "PlayerHand",
+	PlayerHandBusted:        "PlayerHandBusted",
+	PlayerAlreadyStopped:    "PlayerAlreadyStopped",
+	PlayerAlreadyBusted:     "PlayerAlreadyBusted",
+	StoppedDrawing:          "StoppedDrawing",
+	DealerHand:              "DealerHand",
+	DealerBlackjack:         "DealerBlackjack",
+	GameStartHint:           "GameStartHint",
+	GameEnterHint:           "GameEnterHint",
+	TopPlayers:              "TopPlayers",
+}
+
+type MockBag struct {
+	Cards      *mock_services.MockCards
+	Chats      *mock_services.MockChats
+	Games      *mock_services.MockGames
+	Players    *mock_services.MockPlayers
+	Statistics *mock_services.MockStatistics
+	Logger     *mock_services.MockLogger
+	Helper     *mock_handlers.MockTelegramHandlerHelper
+}
+
+func GetHandlerParamsWithMocks(t *testing.T) (HandlerParams, *MockBag) {
+	t.Helper()
+	ctrl := gomock.NewController(t)
+	cardsMock := mock_services.NewMockCards(ctrl)
+	chatsMock := mock_services.NewMockChats(ctrl)
+	gamesMock := mock_services.NewMockGames(ctrl)
+	playersMock := mock_services.NewMockPlayers(ctrl)
+	statisticsMock := mock_services.NewMockStatistics(ctrl)
+	helperMock := mock_handlers.NewMockTelegramHandlerHelper(ctrl)
+	loggerMock := mock_services.NewMockLogger(ctrl)
+	services := &services.Services{
+		Cards:      cardsMock,
+		Chats:      chatsMock,
+		Games:      gamesMock,
+		Players:    playersMock,
+		Statistics: statisticsMock,
+	}
+
+	return HandlerParams{
+			Ctx:      context.Background(),
+			Services: services,
+			Logger:   loggerMock,
+			Messages: messages,
+			Helper:   helperMock,
+		}, &MockBag{
+			Cards:      cardsMock,
+			Chats:      chatsMock,
+			Games:      gamesMock,
+			Players:    playersMock,
+			Statistics: statisticsMock,
+			Logger:     loggerMock,
+			Helper:     helperMock,
+		}
+}
